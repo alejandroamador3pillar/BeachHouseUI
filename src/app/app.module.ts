c0376904782163a70051bcbf58c4df5c1db7606f
Ale <aleamadorq@gmail.com>
2021-03-05 13:41:28 -0600

    Last fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,7 @@ import { DemoComponent } from './demo/demo.component';
 import { SocialLoginModule, SocialAuthServiceConfig } from 'lib';
 import {
   GoogleLoginProvider,
-  FacebookLoginProvider,
-  AmazonLoginProvider,
-  VKLoginProvider,
-  MicrosoftLoginProvider
+  
 } from 'lib';
 
 @NgModule({
@@ -30,26 +27,6 @@ import {
             provider: new GoogleLoginProvider(
               '428648805208-5fsd1d9lq8he4p80ujcc2m5eri1vi1e0.apps.googleusercontent.com'
             ),
-          },
-          {
-            id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('561602290896109'),
-          },
-          {
-            id: AmazonLoginProvider.PROVIDER_ID,
-            provider: new AmazonLoginProvider(
-              'amzn1.application-oa2-client.f074ae67c0a146b6902cc0c4a3297935'
-            ),
-          },
-          {
-            id: VKLoginProvider.PROVIDER_ID,
-            provider: new VKLoginProvider(
-              '7624815'
-            ),
-          },
-          {
-            id: MicrosoftLoginProvider.PROVIDER_ID,
-            provider: new MicrosoftLoginProvider('0611ccc3-9521-45b6-b432-039852002705'),
           }
         ],
       } as SocialAuthServiceConfig,
