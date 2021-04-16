a18dad3a6db3f0f4a2a9174d6a7f6aef08ad2ad9
Geremy Rivera <geremy.rivera@3pillarglobal.com>
2021-04-16 08:58:44 -0600

    Calendar cells background

--- src/app/ui/snack-bar/snack-bar.component.ts
+++ src/app/ui/snack-bar/snack-bar.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
+import {MAT_SNACK_BAR_DATA} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snack-bar',
   templateUrl: './snack-bar.component.html',
   styleUrls: ['./snack-bar.component.css']
 })
-export class SnackBarComponent  {
-
-  constructor() { }
-
-
+export class SnackBarComponent {
+  // constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) { }
 }
