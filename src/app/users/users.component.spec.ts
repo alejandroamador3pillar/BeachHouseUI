3d6c3edcefbe29d9575294fb0c95b10281edb628
Ricardo <ricardo.bolanos@3pillarglobal.com>
2021-04-27 16:28:50 -0600

    Get user from de backend

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -23,3 +23,6 @@ describe('UsersComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,23 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from './users.service';
+import { IUserModel } from '../domain/UserModel';
+import { SocialUser } from 'lib';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { of } from 'rxjs';
+
+const USER_SCHEMA = {
+  "id": "text",
+  "role": "int",
+  "active": "int",
+  "email": "text",
+  "firstName": "text",
+  "lastName": "text",
+  "phone": "text" 
+}
+
+
+
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
-  
-  name: string="Ricardo";
 
-  constructor() { }
+export class UsersComponent implements OnInit {
+  users: IUserModel[] = [];
+  user:  IUserModel;
+  displayedColumns: string[] = ['id','lastName','firstName', 'role', 'active', 'email','phone', '$$edit'];
+  durationInSeconds = 5;
+  dataSchema = USER_SCHEMA;
+ 
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
-  fnMostrar(): void{
-    this.name="Carlos";
+  
+  getUsers(): void {
+    //user.id='112233445566';
+    this.usersService.getUsers()
+      .subscribe(users => this.users = users);
+      console.log(this.users);
   }
 
+  setUser(user: IUserModel): void{
 
+   
 
+    this.usersService.setUsers(user)
+    .subscribe((data) => {
+        console.log(data);
+        this.openSnackBar(data);
+        return true;
+    },
+    (error) => {      
+      console.log(error);
+      this.openSnackBar(error);
+      this.user = null;
+      //return of();
+      return false; 
+    });
+  }
+
+  openSnackBar(data: any) {
+    /* this.snackBar.openFromComponent(ParametersComponent, {
+      duration: this.durationInSeconds * 1000,
+    }); */    
+  }
 }
+
+//----------------------------------------
+
+  
+
+
+
