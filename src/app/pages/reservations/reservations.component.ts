4de1643c63b181d3e3de885707aea1edc4742b32
Geremy Rivera <geremy.rivera@3pillarglobal.com>
2021-05-17 17:57:13 -0600

    reservations advance

--- src/app/pages/reservations/reservations.component.ts
+++ src/app/pages/reservations/reservations.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { ParametersService } from 'src/app/services/service.index';
 import { IParameterModel } from '../../models/parameter.model';
 import { ReserveService } from 'src/app/services/service.index';
-import { SocialUser } from 'lib';
+import { SocialUser, SocialAuthService } from 'lib';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 const USER_SCHEMA = {
   "id": "text",
@@ -24,14 +25,15 @@ export class ReservationsComponent implements AfterViewInit {
   parameters: IParameterModel[] = [];
   parameter: IParameterModel;
   durationInSeconds = 5;
-  userReservations: any[]=[];
+  userReservations: any[] = [];
   dataSource = new MatTableDataSource<any>(this.userReservations);
   loading = true;
   displayedColumns: string[] = ['date', '$$delete'];
   dataSchema = USER_SCHEMA;
   userData: SocialUser;
 
-  constructor(private parametersService: ParametersService, private reservationService: ReserveService) { }
+  constructor(private parametersService: ParametersService, private reservationService: ReserveService, private _snackBar: MatSnackBar,
+    private authService: SocialAuthService) { }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -44,21 +46,29 @@ export class ReservationsComponent implements AfterViewInit {
     this.getReservationsByUser();
   }
 
+  getUserData() {
+    this.authService.authState.subscribe(user => { this.userData = user });
+  }
+
   getParameters(): void {
     this.parametersService.getParameters()
-      .subscribe(parameters => {this.parameters = parameters;
-      this.loading = false;});
+      .subscribe(parameters => {
+        this.parameters = parameters;
+        this.loading = false;
+      });
   }
 
   getReservationsByUser(): void {
     this.reservationService.getReservationsByUser(this.userData.id)
-      .subscribe(userReservations => {this.userReservations = userReservations;
-      this.loading = false;});
+      .subscribe(userReservations => {
+        this.userReservations = userReservations;
+        this.loading = false;
+      });
   }
 
-  cancelReservation(user_id: number, res_id: number){
+  cancelReservation(user_id: number, res_id: number) {
     this.reservationService.cancelReservation(user_id, res_id)
-    .subscribe();
+      .subscribe();
 
   }
 
